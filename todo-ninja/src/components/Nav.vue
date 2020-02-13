80f62fed7ad07daf363ad5ae8d276d325336a7b9
Nilesh <mjn.nilesh@gmail.com>
2020-02-13 13:57:08 +0545

    Toolbars

---
+++ todo-ninja/src/components/Nav.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+      Here
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
--- todo-ninja/src/views/Home.vue
+++ todo-ninja/src/views/Home.vue
@@ -1,13 +1,12 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1>Home Page</h1>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  // import HelloWorld from '../components/HelloWorld'
 
   export default {
-    components: {
-      HelloWorld
-    }
   }
 </script>
