353e4d49e35ec243f38203d3a925412b6edd4e5c
Nilesh <mjn.nilesh@gmail.com>
2020-02-13 14:57:28 +0545

    Padding and margin

--- todo-ninja/src/App.vue
+++ todo-ninja/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app class ="grey lighten-4">
     <Navbar/>
-    <v-content>
+    <v-content class="mx-4 mb-4">
       <router-view></router-view>
     </v-content>
   </v-app>
@@ -13,9 +13,9 @@ export default {
   name: 'App',
   components: { Navbar },
   data () {
-    // return {
-    //   //
-    // }
+    return {
+      
+    }
   }
 }
 </script>
