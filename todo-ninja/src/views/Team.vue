0d04d7d3f62453897f3a534d233862fef6f79f4a
Nilesh <mjn.nilesh@gmail.com>
2020-02-13 16:45:47 +0545

    Avatar

--- todo-ninja/src/views/Team.vue
+++ todo-ninja/src/views/Team.vue
@@ -32,11 +32,11 @@
     data() {
       return {
          team: [
-        { name: 'The Net Ninja', role: 'Web developer' },
-        { name: 'Ryu', role: 'Graphic designer' },
-        { name: 'Chun Li', role: 'Web developer' },
-        { name: 'Gouken', role: 'Social media maverick' },
-        { name: 'Yoshi', role: 'Sales guru'}
+        { name: 'The Net Ninja', role: 'Web developer', avatar: '/avatar-1.png' },
+        { name: 'Ryu', role: 'Graphic designer', avatar: '/avatar-2.png' },
+        { name: 'Chun Li', role: 'Web developer', avatar: '/avatar-3.png' },
+        { name: 'Gouken', role: 'Social media maverick', avatar: '/avatar-4.png' },
+        { name: 'Yoshi', role: 'Sales guru', avatar: '/avatar-5.png'}
       ]
       }
     }
